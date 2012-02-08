aaa35e72f700cf97d627ca9420134ccb70ea2011
Miguel Bejar <bejar37@gmail.com>
2012-02-08 01:43:42 -0500

    initial commit, starting first rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appone::Application.config.secret_token = 'e22f05c705fd6cab47f3f20a4958dfa903550514251ed463dc28c27af96826a0831bc0704ece75f66b7652a4ce03508fc088276dd91e2a46da2477713174153f'
